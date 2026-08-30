2b2897f0ae52cb152dd9688e4c0958d483b04a33
Ian <atozpiece@gmail.com>
2026-08-30 20:38:16 +0300

    fix(contrast): set crisp white heading color for Admissions CTA on About page and clean up unused icon import in FutureReadySection

--- src/components/home/FutureReadySection.jsx
+++ src/components/home/FutureReadySection.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { FaRobot, FaLaptopCode, FaBrain, FaSparkles } from "react-icons/fa";
+import { FaRobot, FaLaptopCode, FaBrain } from "react-icons/fa";
 import { useLanguage } from "../../context/LanguageContext";
 
 const HEADLINES = {
